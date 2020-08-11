e4898bd7a8a5de87213d216fb2dd13d9e1d70503
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-08-11 19:37:48 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,10 +1,11 @@
 class BankAccount
-  attr_accessor :balance
+  attr_accessor :balance, :status
   attr_reader :name
 
   def initialize(name)
     @name = name
     @balance = 1000
+    @status = open
   end
 
 end
