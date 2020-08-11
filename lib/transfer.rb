3d42efbe646d6f6924fd144cef9360cb47e73394
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-08-11 19:54:30 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 class Transfer
   attr_accessor :sender, :receiver, :amount, :status
 
@@ -21,6 +23,7 @@ class Transfer
   end
 
   def execute_transaction
+
     if valid?
   end
 
